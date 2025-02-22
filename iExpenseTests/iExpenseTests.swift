f671ea3f948d1f527822aed9880ca188bb0063b3
Hariom009 <hariomconnects03@gmail.com>
2025-02-22 22:14:36 +0530

    Initial Commit

---
+++ iExpenseTests/iExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTests.swift
+//  iExpenseTests
+//
+//  Created by Hari's Mac on 22.02.2025.
+//
+
+import Testing
+@testable import iExpense
+
+struct iExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
